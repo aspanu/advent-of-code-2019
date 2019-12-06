d05ee6f0911cc1bf14d94a85041241298a80aff1
Adrian Spanu <adri@nspanu.ca>
2019-12-06 20:30:11 +0530

    Starting on day 5 and adding the day6 question.

--- day5.py
+++ day5.py
@@ -44,7 +44,56 @@ It will then perform a series of diagnostic tests confirming that various parts 
 Finally, the program will output a diagnostic code and immediately halt. This final output isn't an error; an output followed immediately by a halt means the program finished. If all outputs were zero except the diagnostic code, the diagnostic program ran successfully.
 
 After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?
-
+--> 
 '''
+
+opcode_lengths = {'1': 4, '2': 4, '3': 2, '4': 2}
+
+def intcode_computer_from_day2(instructions: list) -> list:
+    current_opcode = instructions[0]
+    position = 0
+    while current_opcode != '99':
+        value1 = int(instructions[int(instructions[position + 1])])
+        value2 = int(instructions[int(instructions[position + 2])])
+        if current_opcode == '1':
+            newValue = value1 + value2
+        elif current_opcode == '2':
+            newValue = value1 * value2
+
+        instructions[int(instructions[position + 3])] = str(newValue)
+        position += 4
+        current_opcode = instructions[position]
+
+    return instructions
+
+def haveAddition(instructions: list, position: int) -> int:
+    pass
+
+def haveMultiplication(instructions: list, position: int) -> int:
+    pass
+
+def addInput(instructions: list, position: int, input: int) -> int:
+    pass
+
+def getOutput(instructions: list, position: int) -> int:
+    pass
+
+# TODO: Pass in instructions that are a list of integers
+def intcode_computer_with_test(instructions: list, input: int) -> None:
+    current_opcode = instructions[0]
+    position = 0
+    while current_opcode != 99:
+        if current_opcode == 1:
+            new_position = haveAddition(instructions, position)
+        elif current_opcode == 2:
+            new_position = haveMultiplication(instructions, position)
+        elif current_opcode == 3:
+            new_position = addInput(instructions, position, input)
+        elif current_opcode == 4:
+            new_position = getOutput(instructions, position)
+
+        else:
+            raise NameError('This opcode has the wrong name, expected 1 - 4, but got {}'.format(current_opcode))
+        # TODO: Convert parameter modes to single op code
 
 main_input = "3,225,1,225,6,6,1100,1,238,225,104,0,1102,31,68,225,1001,13,87,224,1001,224,-118,224,4,224,102,8,223,223,1001,224,7,224,1,223,224,223,1,174,110,224,1001,224,-46,224,4,224,102,8,223,223,101,2,224,224,1,223,224,223,1101,13,60,224,101,-73,224,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1101,87,72,225,101,47,84,224,101,-119,224,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1101,76,31,225,1102,60,43,225,1102,45,31,225,1102,63,9,225,2,170,122,224,1001,224,-486,224,4,224,102,8,223,223,101,2,224,224,1,223,224,223,1102,29,17,224,101,-493,224,224,4,224,102,8,223,223,101,1,224,224,1,223,224,223,1102,52,54,225,1102,27,15,225,102,26,113,224,1001,224,-1560,224,4,224,102,8,223,223,101,7,224,224,1,223,224,223,1002,117,81,224,101,-3645,224,224,4,224,1002,223,8,223,101,6,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,8,226,677,224,102,2,223,223,1005,224,329,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,344,101,1,223,223,108,677,226,224,102,2,223,223,1006,224,359,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,374,101,1,223,223,1007,226,677,224,102,2,223,223,1005,224,389,101,1,223,223,8,677,677,224,102,2,223,223,1006,224,404,1001,223,1,223,1007,677,677,224,1002,223,2,223,1006,224,419,101,1,223,223,1108,677,677,224,1002,223,2,223,1005,224,434,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,449,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,464,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,7,677,677,224,102,2,223,223,1006,224,494,1001,223,1,223,1107,677,226,224,102,2,223,223,1005,224,509,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,524,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,539,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,569,101,1,223,223,1008,226,677,224,102,2,223,223,1005,224,584,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,599,101,1,223,223,1007,226,226,224,1002,223,2,223,1005,224,614,101,1,223,223,1107,226,226,224,1002,223,2,223,1006,224,629,101,1,223,223,107,677,226,224,1002,223,2,223,1005,224,644,1001,223,1,223,1008,226,226,224,1002,223,2,223,1006,224,659,101,1,223,223,108,677,677,224,1002,223,2,223,1005,224,674,1001,223,1,223,4,223,99,226"
